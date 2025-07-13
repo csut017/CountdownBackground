99f3d639f8fbbe450a5b9d66bad4d9ccdaaa6384
Craig Sutherland <cj.sutherland@auckland.ac.nz>
2025-07-14 07:31:39 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using Humanizer;
+using Humanizer.Localisation;
+
+namespace CountdownBackground;
+
+internal class Program
+{
+    private static string GenerateNewWallpaper(string imagePath, TimeSpan daysRemaining)
+    {
+        var folder = Path.GetDirectoryName(imagePath);
+        if (string.IsNullOrEmpty(folder)) throw new Exception("path is not rooted");
+        var extension = Path.GetExtension(imagePath);
+
+        var text = "T-" + daysRemaining.Humanize(maxUnit: TimeUnit.Day);
+        var newPath = Path.Combine(folder, $"{text}{extension}");
+#pragma warning disable CA1416
+        using var original = new Bitmap(imagePath);
+        using var image = new Bitmap(original.Width, original.Height);
+        using var graphics = Graphics.FromImage(image);
+        graphics.DrawImage(original, 0, 0, original.Width, original.Height);
+        using var font = new Font("Roboto", 120);
+        graphics.DrawString(text, font, new SolidBrush(Color.Black), 710, 20);
+        graphics.DrawString(text, font, new SolidBrush(Color.White), 700, 10);
+        graphics.Flush();
+
+        image.Save(newPath, ImageFormat.Png);
+#pragma warning restore CA1416
+        return newPath;
+    }
+
+    static void Main(string[] args)
+    {
+        const string imagePath = @"C:\Users\csut017\OneDrive - The University of Auckland\Pictures\Live-Fearlessly-desktop-wallpaper-en.png";
+        var targetDate = new DateTime(2025, 7, 21);
+        var daysRemaining = targetDate - DateTime.Today;
+        Console.WriteLine("Generating wallpaper");
+        var newPath = GenerateNewWallpaper(imagePath, daysRemaining);
+
+        Console.WriteLine("Setting wallpaper");
+        WallpaperUtility.SetWallpaper(newPath);
+        Console.WriteLine("Wallpaper update completed");
+    }
+}
---
+++ WallpaperUtility.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace CountdownBackground;
+
+internal static class WallpaperUtility
+{
+    private static readonly uint SPI_SETDESKWALLPAPER = 0x14;
+
+    private static readonly uint SPIF_SENDWININICHANGE = 0x02;
+
+    private static readonly uint SPIF_UPDATEINIFILE = 0x01;
+
+    public static void SetWallpaper(string path)
+    {
+#pragma warning disable CA1416
+        var key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+        if (key != null)
+        {
+            key.SetValue(@"WallpaperStyle", 2.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+        else
+        {
+            throw new Exception("Could not open registry key");
+        }
+#pragma warning restore CA1416
+
+        var result = SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+        if (result != 1)
+        {
+            throw new Exception("SystemParametersInfo failed");
+        }
+    }
+
+    [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+    private static extern int SystemParametersInfo(uint action, uint uParam, string vParam, uint winIni);
+}
